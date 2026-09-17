606df9f33071781a3d1dc69e93c44d3b40e9574c
Yitzi Weiner <yitziw123@gmail.com>
2026-09-17 12:02:03 +0300

    Added login endpoint

--- server/controllers/profileControllers.js
+++ server/controllers/profileControllers.js
@@ -1,4 +1,4 @@
-import { registerService } from "../services/profileServices.js";
+import { loginService, registerService } from "../services/profileServices.js";
 
 export const register = async (req, res) => {
     try {
@@ -38,6 +38,41 @@ export const register = async (req, res) => {
         res.status(err.status).json({
             success: false,
             error: err.message,
+        });
+    }
+};
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email) {
+            return res.status(400).json({
+                success: false,
+                error: "Email is required",
+            });
+        }
+
+        if (!password) {
+            return res.status(400).json({
+                success: false,
+                error: "Password is required",
+            });
+        }
+
+        const token = await loginService(email, password);
+
+        res.json({
+            success: true,
+            message: "User logged in successfully",
+            data: {
+                token,
+            },
+        });
+    } catch (error) {
+        res.status(error.status).json({
+            success: false,
+            error: error.message,
         });
     }
 };
--- server/services/profileServices.js
+++ server/services/profileServices.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import Profile from "../models/user.js";
 
 export const registerService = async (name, email, phone, password) => {
@@ -6,7 +7,7 @@ export const registerService = async (name, email, phone, password) => {
 
     const findProfile = await Profile.findOne({ email: trimmedEmail });
 
-    if (!findProfile) {
+    if (findProfile) {
         const error = new Error("Email not found");
         error.status = 404;
         throw error;
@@ -20,4 +21,32 @@ export const registerService = async (name, email, phone, password) => {
         phone,
         password: hashedPassword,
     });
+};
+
+export const loginService = async (email, password) => {
+    const trimmedEmail = email.toLowerCase().trim();
+
+    const profile = await Profile.findOne({ email: trimmedEmail });
+
+    if (!profile) {
+        const error = new Error("Profile not found");
+        error.status = 404;
+        throw error;
+    }
+
+    const matchPassword = await bcrypt.compare(password, profile.password);
+
+    if (!matchPassword) {
+        const error = new Error("Invalid Password");
+        error.status = 401;
+        throw error;
+    }
+
+    const token = jwt.sign(
+        { profileId: profile._id.toString() },
+        process.env.JWT_SECRET,
+        { expiresIn: "1h" },
+    );
+
+    return token;
 };
